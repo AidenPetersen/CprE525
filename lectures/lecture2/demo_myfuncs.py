079da821a2b56a8d134655e7a77e23153f01795e
Aiden Petersen <aidenpetersen@protonmail.com>
2025-01-30 11:57:58 -0600

    Updated demo funcs to check agains library implementations

--- lectures/lecture2/demo_myfuncs.py
+++ lectures/lecture2/demo_myfuncs.py
@@ -1,12 +1,17 @@
+import math
+
 def sqrtIter(x, S: float) -> float:
     return 0.5 * (x + S/x)
 
+# Takes the square root of a number using iterative method
+# Uses sqrtIter as a helper function to perform one iteration
 def sqrt(num: float) -> float:
     x0 = 600
     x_next = x0
     while True:
         x_old = x_next
         x_next = sqrtIter(x_next, num)
+        # Checks that algorithm is complete if previous round is less than delta
         if abs(x_old - x_next) < 1.0e-14:
             break
     
@@ -15,12 +20,13 @@ def sqrt(num: float) -> float:
 # factorial
 def factorial(x: int) -> int:
     acc = 1
+    # Multiplies each iteration index
     for i in range(1, x+1):
         acc *= i
     return acc
 
 # e^x
-def ex(x: float) -> float:
+def exp(x: float) -> float:
     acc = 1.0
     i = 1
     while True:
@@ -37,12 +43,12 @@ def ln(x: float) -> float:
     i = 0
     while True:
         s_old = s
-        s = s - 1 + x * ex(-s) 
+        s = s - 1 + x * exp(-s) 
         if abs(s - s_old) < 1e-14:
             break
     return s
 
-print(f"sqrt(10): {sqrt(10)}")
-print(f"factorial(10): {factorial(10)}")
-print(f"ex(3): {ex(3)}")
-print(f"ln(3): {ln(3)}")
+print(f"sqrt(10): Expected: {math.sqrt(10)} Implementation: {sqrt(10)}")
+print(f"factorial(10): Expected: {math.factorial(10)} Implementation: {factorial(10)}")
+print(f"ex(3): Expected: {math.exp(3)} Implementation: {exp(3)}")
+print(f"ln(3): Expected: {math.log(3, math.e)} Implementation: {ln(3)}")
