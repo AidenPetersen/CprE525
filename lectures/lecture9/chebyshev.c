4ac8238bb3c6deaf3b27f7eceeb5cae6e5877305
Aiden Petersen <aidenpetersen@protonmail.com>
2025-02-18 14:32:15 -0600

    Updated Chebyshiv

--- lectures/lecture9/chebyshev.c
+++ lectures/lecture9/chebyshev.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #define SIZE 100
@@ -7,59 +7,57 @@
 double xs[SIZE];
 double ys[SIZE];
 double c0[] = {1.0};
-double c1[] = {1.0, 0};
-double c2[] = {2, 0, -1};
-double c3[] = {4,0, -3 ,0};
-double c4[] = {8,0,-8,0,1};
-double c5[] = {16,0,-20,0,5,0};
+double c1[] = {0, 1.0};
+double c2[] = {-1, 0, 2};
+double c3[] = {0, -3, 0, 4};
+double c4[] = {1, 0, -8, 0, 8};
+double c5[] = {0, 5, 0, -20, 0, 16};
 
-void sample_poly(double* polynomial, int dim){
-    for(int i = 0; i < SIZE; i++){
-
-        xs[i] = (double) i / 20;
-        ys[i] = 0;
-        for(int j = 0; j < dim; j++){
-            ys[i] += polynomial[j] * pow(xs[i], (double)j);
-        }
-    }
+double sample_poly(double x, double *polynomial, int dim) {
+  double y = 0;
+  for (int j = 0; j < dim; j++) {
+    y += polynomial[j] * pow(x, (double)j);
+  }
+  return y;
 }
 
-double* chebyshev(int n){
-    switch (n) {
-        case 0:
-            return c0;
-            break;
-        case 1:
-            return c1;
-            break;
-        case 2:
-            return c2;
-            break;
-        case 3:
-            return c3;
-            break;
-        case 4:
-            return c4;
-            break;
-        case 5:
-            return c5;
-            break;
-        default:
-            printf("INVALID N TOO BIG OR SMALL");
-            exit(1);
-        }
+double chebyshev(int n, double x) {
+  double y = 0;
+  switch (n) {
+  case 5:
+    y += sample_poly(x, c5, 6);
+  case 4:
+    y += sample_poly(x, c4, 5);
+  case 3:
+    y += sample_poly(x, c3, 4);
+  case 2:
+    y += sample_poly(x, c2, 3);
+  case 1:
+    y += sample_poly(x, c1, 2);
+  case 0:
+    y += sample_poly(x, c0, 1);
+    break;
+  default:
+    printf("INVALID N TOO BIG OR SMALL");
+    exit(1);
+  }
+  return y;
 }
 
+void load_out(int n) {
+  for (int i = 0; i < SIZE; i++) {
+    xs[i] = (double)i / 20;
+    ys[i] = chebyshev(n, xs[i]);
+  }
+}
 
-
-int main(int argc, char* argv[]){
-    int N = atoi(argv[1]);
-    double* poly = chebyshev(N);
-    int size = N+1;
-    sample_poly(poly, size);
-    FILE* fp = fopen("chebyshev.txt", "w");
-    for(int i = 0; i < SIZE; i++){
-        fprintf(fp, "%lf,%lf\n", xs[i], ys[i]);
-    }
-    return system("python3 viz.py");
+int main(int argc, char *argv[]) {
+  int N = atoi(argv[1]);
+  load_out(N);
+  FILE *fp = fopen("chebyshev.txt", "w");
+  for (int i = 0; i < SIZE; i++) {
+    fprintf(fp, "%lf,%lf\n", xs[i], ys[i]);
+  }
+  fclose(fp);
+  return system("python3 viz.py");
 }
